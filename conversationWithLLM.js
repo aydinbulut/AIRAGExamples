e36770a1f18bc8431ebf4d471e78f119d91180f9
Aydin Bulut <aydin_bulut@epam.com>
2025-04-21 19:51:29 +0200

    Make LLM params visible and play with it

--- conversationWithLLM.js
+++ conversationWithLLM.js
@@ -2,8 +2,16 @@ import { ChatOllama } from "@langchain/ollama";
 import { ConversationChain } from "langchain/chains";
 import readline from 'readline';
 
-const model = new ChatOllama({model: 'llama3.2', temperature: 0, streaming: true});
-const chain = new ConversationChain({ llm: model });
+const model = new ChatOllama(
+    {
+        model: 'llama3.2',
+        temperature: 0.5, // 0 for deterministic output, 1 for more creative output
+        streaming: true, // Set to true for streaming output
+        topK: 5, // Number of top tokens to sample from 1-100
+        topP: 0.5, // Cumulative probability for sampling
+        maxTokens: 1 // Maximum number of tokens to generate
+    });
+const chain = new ConversationChain({ llm: model, streaming: true });
 
 // Ask user in console to provide prompt arbitrarily
 const rl = readline.createInterface({
